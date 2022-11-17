f4dcfaf4142add22d3283348376491bb0f8a0880
krzysztof baranski <krisbaranski@gmail.com>
2022-11-17 22:25:48 +0100

    feat: create new slideshow component and copy code from app-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  images = ['image0.jpg', 'image1.jpg', 'image2.jpg'];
-  currentImage = 0;
-  showImage = true;
-
-  ngOnInit() {
-    this.updateImage();
-  }
-
-  updateImage() {
-    setInterval(() => {
-      this.currentImage++;
-      this.currentImage = this.currentImage % this.images.length;
-      this.showImage = false;
-
-      setTimeout(() => {
-        this.showImage = true;
-      }, 10);
-    }, 8000);
-  }
-}
+export class AppComponent {}
---
+++ src/app/slideshow/slideshow.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slideshow',
+  templateUrl: './slideshow.component.html',
+  styleUrls: ['./slideshow.component.scss'],
+})
+export class SlideshowComponent implements OnInit {
+  images = ['image0.jpg', 'image1.jpg', 'image2.jpg'];
+  headlines = [
+    'bring your voice to the next level',
+    'sing like a bird',
+    'make your voice strong and confindent',
+  ];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 5);
+    }, 8000);
+  }
+}
