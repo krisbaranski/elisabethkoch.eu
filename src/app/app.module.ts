3c3879d0b73be91070ab1180e938738309b8fe71
krzysztof baranski <krisbaranski@gmail.com>
2022-11-21 21:19:55 +0100

    refactor: add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, SlideshowComponent],
+  declarations: [AppComponent, SlideshowComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
