8daa851c99e07f37bfb5a86db1ef18a9d74d13b5
krzysztof baranski <krisbaranski@gmail.com>
2022-11-23 13:35:20 +0100

    refactor: accept changes after merging from branch main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,29 +6,6 @@ import { Router } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-<<<<<<< HEAD
-export class AppComponent implements OnInit {
-  images = ['image0.jpg', 'image1.jpg', 'image2.jpg'];
-  currentImage = 0;
-  showImage = true;
-
-  ngOnInit() {
-    this.updateImage();
-  }
-
-  updateImage() {
-    setInterval(() => {
-      this.currentImage++;
-      this.currentImage = this.currentImage % this.images.length;
-      this.showImage = false;
-
-      setTimeout(() => {
-        this.showImage = true;
-      }, 5);
-    }, 8000);
-  }
-=======
 export class AppComponent {
   constructor(public router: Router) {}
->>>>>>> main
 }
