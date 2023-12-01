aeb18e3c01b690f151bbc697d8d17891b07c4c7b
krzysztof baranski <krisbaranski@gmail.com>
2023-12-01 20:54:02 +0100

    chore: add new component

---
+++ src/app/aboutme/aboutme.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aboutme',
+  templateUrl: './aboutme.component.html',
+  styleUrls: ['./aboutme.component.scss'],
+})
+export class AboutmeComponent implements OnInit {
+  @Input() darkMode = true;
+  images = ['aboutme.jpg', 'aboutme2.jpg'];
+  headlines = ['your professional coaching', 'brings you to the top'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 15);
+    }, 8000);
+  }
+}
