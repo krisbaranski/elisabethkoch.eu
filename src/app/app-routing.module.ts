546d4dc83fed29a70b99e8d29a980345597f3d35
krzysztof baranski <krisbaranski@gmail.com>
2023-12-27 23:07:39 +0100

    refactor: add new link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { SlideshowComponent } from './slideshow/slideshow.component';
 import { ImpressumComponent } from './impressum/impressum.component';
 import { PrivacyComponent } from './privacy/privacy.component';
-// import { ContactComponent } from './contact/contact.component';
+import { ContactComponent } from './contact/contact.component';
 import { AboutmeComponent } from './aboutme/aboutme.component';
 import { AppointmentComponent } from './appointment/appointment.component';
 
 const routes: Routes = [
   { path: '', component: SlideshowComponent },
   { path: 'aboutme', component: AboutmeComponent },
-  // { path: 'contact', component: ContactComponent },
   { path: 'appointment', component: AppointmentComponent },
+  { path: 'contact', component: ContactComponent },
   { path: 'impressum', component: ImpressumComponent },
   { path: 'privacy', component: PrivacyComponent },
 ];
