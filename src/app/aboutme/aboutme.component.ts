7d70f44d08fef2c1ff78fa573b5f6bd61f8cadb0
krzysztof baranski <krisbaranski@gmail.com>
2024-05-27 21:43:04 +0200

    style: implement new background animation

--- src/app/aboutme/aboutme.component.ts
+++ src/app/aboutme/aboutme.component.ts
@@ -8,6 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class AboutmeComponent implements OnInit {
   @Input() darkMode = true;
   images = ['aboutme.jpg', 'aboutme2.jpg'];
+  images2 = ['bg-ochre.png', 'bg-grey.png'];
   headlines = ['your professional coaching', 'brings you to the top'];
   currentImage = 0;
   showImage = true;
