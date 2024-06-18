b92feddb2053da20197dc0388df5f60d9535a662
krzysztof baranski <krisbaranski@gmail.com>
2024-06-18 22:19:32 +0200

    chore: update project name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/example_page'),
+      dir: require('path').join(__dirname, './coverage/elisabethkoch.eu'),
       subdir: '.',
       reporters: [
         { type: 'html' },
