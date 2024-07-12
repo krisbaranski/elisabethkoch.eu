83492df3606713c5abf9befd719af9326a8a0e74
Krzysztof Baranski <krisbaranski@gmail.com>
2024-07-12 21:39:54 +0200

    refactor: delete offer link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { NgModule } from "@angular/core"
+import { BrowserModule } from "@angular/platform-browser"
+import { HashLocationStrategy, LocationStrategy } from "@angular/common"
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SlideshowComponent } from './slideshow/slideshow.component';
-import { HeaderComponent } from './header/header.component';
+import { AppRoutingModule } from "./app-routing.module"
+import { AppComponent } from "./app.component"
+import { SlideshowComponent } from "./slideshow/slideshow.component"
+import { HeaderComponent } from "./header/header.component"
 // import { ImpressumComponent } from './impressum/impressum.component';
 // import { PrivacyComponent } from './privacy/privacy.component';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-// import { FooterComponent } from './footer/footer.component';
+import { TranslateModule, TranslateLoader } from "@ngx-translate/core"
+import { TranslateHttpLoader } from "@ngx-translate/http-loader"
+import { HttpClientModule, HttpClient } from "@angular/common/http"
+import { FooterComponent } from "./footer/footer.component"
 // import { ContactComponent } from './contact/contact.component';
 // import { AboutmeComponent } from './aboutme/aboutme.component';
 // import { AppointmentHomeComponent } from './appointment-home/appointment-home.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
+import { MatToolbarModule } from "@angular/material/toolbar"
+import { MatIconModule } from "@angular/material/icon"
+import { MatSidenavModule } from "@angular/material/sidenav"
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
+  return new TranslateHttpLoader(http)
 }
 
 @NgModule({
@@ -30,9 +30,9 @@ export function HttpLoaderFactory(http: HttpClient) {
     AppComponent,
     SlideshowComponent,
     HeaderComponent,
+    FooterComponent,
     // ImpressumComponent,
     // PrivacyComponent,
-    // FooterComponent,
     // ContactComponent,
     // AboutmeComponent,
     // AppointmentHomeComponent,
@@ -42,7 +42,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     AppRoutingModule,
     HttpClientModule,
     TranslateModule.forRoot({
-      defaultLanguage: 'de',
+      defaultLanguage: "de",
       loader: {
         provide: TranslateLoader,
         useFactory: HttpLoaderFactory,
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,14 +1,21 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core"
+import { TranslateService } from "@ngx-translate/core"
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss'],
+  selector: "app-footer",
+  templateUrl: "./footer.component.html",
+  styleUrls: ["./footer.component.scss"],
 })
 export class FooterComponent implements OnInit {
-  @Input() darkMode = true;
+  @Input() darkMode = true
 
-  constructor() {}
+  constructor(public translate: TranslateService) {
+    // this language will be used as a fallback when a translation isn't found in the current language
+    translate.setDefaultLang("de")
+
+    // the lang to use, if the lang isn't available, it will use the current loader to get them
+    translate.use("en")
+  }
 
   ngOnInit(): void {}
 }
