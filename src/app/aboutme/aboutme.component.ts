ac285ff35aae32ea538cdefb26c5f5e51d2adf41
Krzysztof Baranski <krisbaranski@gmail.com>
2024-07-16 16:22:04 +0200

    chore: deploy setting

--- src/app/aboutme/aboutme.component.ts
+++ src/app/aboutme/aboutme.component.ts
@@ -1,31 +1,41 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core"
+import { TranslateService } from "@ngx-translate/core"
 
 @Component({
-  selector: 'app-aboutme',
-  templateUrl: './aboutme.component.html',
-  styleUrls: ['./aboutme.component.scss'],
+  selector: "app-aboutme",
+  templateUrl: "./aboutme.component.html",
+  styleUrls: ["./aboutme.component.scss"],
 })
 export class AboutmeComponent implements OnInit {
-  @Input() darkMode = true;
-  images = ['aboutme.jpg', 'aboutme2.jpg'];
-  images2 = ['bg-ochre.png', 'bg-grey.png'];
-  headlines = ['aboutme.slide_1', 'aboutme.slide_2'];
-  currentImage = 0;
-  showImage = true;
+  @Input() darkMode = true
+
+  constructor(public translate: TranslateService) {
+    // this language will be used as a fallback when a translation isn't found in the current language
+    translate.setDefaultLang("de")
+
+    // the lang to use, if the lang isn't available, it will use the current loader to get them
+    translate.use("en")
+  }
+
+  images = ["aboutme.jpg", "aboutme2.jpg"]
+  images2 = ["bg-ochre.png", "bg-grey.png"]
+  headlines = ["aboutme.slide_1", "aboutme.slide_2"]
+  currentImage = 0
+  showImage = true
 
   ngOnInit() {
-    this.updateImage();
+    this.updateImage()
   }
 
   updateImage() {
     setInterval(() => {
-      this.currentImage++;
-      this.currentImage = this.currentImage % this.images.length;
-      this.showImage = false;
+      this.currentImage++
+      this.currentImage = this.currentImage % this.images.length
+      this.showImage = false
 
       setTimeout(() => {
-        this.showImage = true;
-      }, 15);
-    }, 8000);
+        this.showImage = true
+      }, 15)
+    }, 8000)
   }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { SlideshowComponent } from './slideshow/slideshow.component';
+import { NgModule } from "@angular/core"
+import { RouterModule, Routes } from "@angular/router"
+import { SlideshowComponent } from "./slideshow/slideshow.component"
+import { AboutmeComponent } from "./aboutme/aboutme.component"
 // import { ImpressumComponent } from './impressum/impressum.component';
 // import { PrivacyComponent } from './privacy/privacy.component';
 // import { ContactComponent } from './contact/contact.component';
-// import { AboutmeComponent } from './aboutme/aboutme.component';
 // import { AppointmentHomeComponent } from './appointment-home/appointment-home.component';
 
 const routes: Routes = [
-  { path: '', component: SlideshowComponent },
-  // { path: 'aboutme', component: AboutmeComponent },
+  { path: "", component: SlideshowComponent },
+  { path: "aboutme", component: AboutmeComponent },
   // { path: 'appointment-home', component: AppointmentHomeComponent },
   // { path: 'contact', component: ContactComponent },
   // { path: 'impressum', component: ImpressumComponent },
   // { path: 'privacy', component: PrivacyComponent },
-];
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: false })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
