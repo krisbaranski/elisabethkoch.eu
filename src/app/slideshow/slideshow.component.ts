19626aeddc471bfef5a70d2df048bb9f641b3de7
Krzysztof Baranski <krisbaranski@gmail.com>
2024-07-17 23:56:17 +0200

    style: home page slideshow padding, line-height, boxes position, change box and font color

--- src/app/slideshow/slideshow.component.ts
+++ src/app/slideshow/slideshow.component.ts
@@ -1,32 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import { Component, OnInit } from "@angular/core"
+import { TranslateService } from "@ngx-translate/core"
 
 @Component({
-  selector: 'app-slideshow',
-  templateUrl: './slideshow.component.html',
-  styleUrls: ['./slideshow.component.scss'],
+  selector: "app-slideshow",
+  templateUrl: "./slideshow.component.html",
+  styleUrls: ["./slideshow.component.scss"],
 })
 export class SlideshowComponent implements OnInit {
   constructor(private translate: TranslateService) {}
-  images = ['nature.png', 'sparcle.jpg', 'water.jpg'];
-  headlines = ['home.slide_1', 'home.slide_2', 'home.slide_3'];
+  images = ["nature.png", "sparcle.jpg", "water.jpg"]
+  headlines = ["home.slide_1", "home.slide_2", "home.slide_3"]
 
-  currentImage = 0;
-  showImage = true;
+  currentImage = 0
+  showImage = true
 
   ngOnInit() {
-    this.updateImage();
+    this.updateImage()
   }
 
   updateImage() {
     setInterval(() => {
-      this.currentImage++;
-      this.currentImage = this.currentImage % this.images.length;
-      this.showImage = false;
+      this.currentImage++
+      this.currentImage = this.currentImage % this.images.length
+      this.showImage = false
 
       setTimeout(() => {
-        this.showImage = true;
-      }, 15);
-    }, 8000);
+        this.showImage = true
+      }, 15)
+    }, 10000)
   }
 }
