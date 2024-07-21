c41bac878c2f6fee218f0ed5356f3be5b9a88058
Krzysztof Baranski <krisbaranski@gmail.com>
2024-07-22 01:01:25 +0200

    style: adjust text for mobile

--- src/app/termine/termine.component.ts
+++ src/app/termine/termine.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, OnInit } from "@angular/core"
-import { TranslateService } from "@ngx-translate/core"
 
 @Component({
   selector: "app-termine",
@@ -9,13 +8,7 @@ import { TranslateService } from "@ngx-translate/core"
 export class TermineComponent implements OnInit {
   @Input() darkMode = true
 
-  constructor(public translate: TranslateService) {
-    // this language will be used as a fallback when a translation isn't found in the current language
-    translate.setDefaultLang("de")
-
-    // the lang to use, if the lang isn't available, it will use the current loader to get them
-    translate.use("en")
-  }
+  constructor() {}
 
   ngOnInit(): void {}
 }
