06fcc12e38ac79a3b8fcd8d883a6b307d72a5f5c
Krzysztof Baranski <krisbaranski@gmail.com>
2024-07-21 17:06:39 +0200

    refactor: add new links, submenu, anchors, ids

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, OnInit } from "@angular/core"
+import { Component, Input, OnInit, ViewChild } from "@angular/core"
 import { TranslateService } from "@ngx-translate/core"
+import { Router } from "@angular/router"
+import { MatSidenav } from "@angular/material/sidenav"
 
 @Component({
   selector: "app-header",
@@ -7,9 +9,10 @@ import { TranslateService } from "@ngx-translate/core"
   styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent implements OnInit {
+  @ViewChild("sidenav") sidenav: MatSidenav
   @Input() darkMode = true
 
-  constructor(public translate: TranslateService) {
+  constructor(public translate: TranslateService, private router: Router) {
     // this language will be used as a fallback when a translation isn't found in the current language
     translate.setDefaultLang("en")
 
@@ -18,4 +21,20 @@ export class HeaderComponent implements OnInit {
   }
 
   ngOnInit(): void {}
+
+  goToPart(fragment: any) {
+    this.router.navigateByUrl("profile#" + fragment)
+  }
+
+  onProfileClick(event: Event) {
+    event.stopImmediatePropagation()
+  }
+
+  navigateAndClose(route: string) {
+    // Navigate to the desired route
+    this.router.navigate([route]).then(() => {
+      // Close the sidebar after navigation is complete
+      this.sidenav.close()
+    })
+  }
 }
