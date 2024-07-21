f5e886222e0c4105b53902c8a0b8aff4eaf36a89
Krzysztof Baranski <krisbaranski@gmail.com>
2024-07-21 17:04:43 +0200

    refactor: add new component

---
+++ src/app/facts/facts.component.ts
@@ -0,0 +1,28 @@
+import { Component } from "@angular/core"
+import { Router } from "@angular/router"
+import { TranslateService } from "@ngx-translate/core"
+
+@Component({
+  selector: "app-facts",
+  templateUrl: "./facts.component.html",
+  styleUrl: "./facts.component.scss",
+})
+export class FactsComponent {
+  text_2: string[]
+  text_3: string[]
+  text_4: string[]
+
+  constructor(public router: Router, public translate: TranslateService) {
+    this.translate.get("facts.text_2").subscribe((res: string[]) => {
+      this.text_2 = res
+    })
+
+    this.translate.get("facts.text_3").subscribe((res: string[]) => {
+      this.text_3 = res
+    })
+
+    this.translate.get("facts.text_4").subscribe((res: string[]) => {
+      this.text_4 = res
+    })
+  }
+}
---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core"
+import { FactsComponent } from "../facts/facts.component"
+
+@Component({
+  selector: "app-profile",
+  templateUrl: "./profile.component.html",
+  styleUrl: "./profile.component.scss",
+})
+export class ProfileComponent {}
