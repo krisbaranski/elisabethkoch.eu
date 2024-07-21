f0700c43a373d1a801486854802aa6d8f4429e96
Krzysztof Baranski <krisbaranski@gmail.com>
2024-07-21 17:08:52 +0200

    refactor: add ids for anchor links

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -12,12 +12,6 @@ export class WorkComponent {
   text_8: string[]
 
   constructor(public router: Router, public translate: TranslateService) {
-    // this language will be used as a fallback when a translation isn't found in the current language
-    translate.setDefaultLang("en")
-
-    // the lang to use, if the lang isn't available, it will use the current loader to get them
-    translate.use("de")
-
     this.translate.get("work.text_7").subscribe((res: string[]) => {
       this.text_7 = res
     })
