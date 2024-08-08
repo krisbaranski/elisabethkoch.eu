f65c568c475f9de8117b91995d2f417406009b8b
Kris Baranski <75210163+krisbaranski@users.noreply.github.com>
2024-08-08 17:08:59 +0200

    chore: add new components

---
+++ src/app/trainings/trainings.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trainings',
+  templateUrl: './trainings.component.html',
+  styleUrl: './trainings.component.scss',
+})
+export class TrainingsComponent {}
