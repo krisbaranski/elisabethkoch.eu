0aa067967e0037a78541e33ce62f92403c340283
Kris Baranski <75210163+krisbaranski@users.noreply.github.com>
2024-09-02 16:09:36 +0200

    refactor: separate slide from aboutme component

--- src/app/aboutme/aboutme.component.ts
+++ src/app/aboutme/aboutme.component.ts
@@ -1,33 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-aboutme',
   templateUrl: './aboutme.component.html',
   styleUrls: ['./aboutme.component.scss'],
 })
-export class AboutmeComponent implements OnInit {
-  @Input() darkMode = true;
-
-  constructor() {}
-
-  images = ['me_green.png', 'aboutme2.jpg'];
-  headlines = ['', ''];
-  currentImage = 0;
-  showImage = true;
-
-  ngOnInit() {
-    this.updateImage();
-  }
-
-  updateImage() {
-    setInterval(() => {
-      this.currentImage++;
-      this.currentImage = this.currentImage % this.images.length;
-      this.showImage = false;
-
-      setTimeout(() => {
-        this.showImage = true;
-      }, 15);
-    }, 8000);
-  }
-}
+export class AboutmeComponent {}
