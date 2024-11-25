16803bcc0e216dd2c81bb47f81eb769ff61bdd87
Kris Baranski <75210163+krisbaranski@users.noreply.github.com>
2024-11-25 19:27:46 +0100

    refactor: restructure components

--- src/app/boxes-home/boxes-home.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-boxes-home',
-  templateUrl: './boxes-home.component.html',
-  styleUrl: './boxes-home.component.scss',
-})
-export class BoxesHomeComponent {}
