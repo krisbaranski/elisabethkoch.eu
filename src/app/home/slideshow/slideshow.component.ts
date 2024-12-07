cf1bbc8d5eec0d12b29daa89df53671f332e0920
Kris Baranski <75210163+krisbaranski@users.noreply.github.com>
2024-12-07 12:13:26 +0100

    refactor: adjust fonts, simplify, app components

--- src/app/home/slideshow/slideshow.component.ts
+++ src/app/home/slideshow/slideshow.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-slideshow',
@@ -7,11 +6,11 @@ import { Router } from '@angular/router';
   styleUrls: ['./slideshow.component.scss'],
 })
 export class SlideshowComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor() {}
   images = ['aboutme1.jpg'];
   text_big = ['home.slide_1'];
   headline = ['home.title_1'];
-  text_small = ['home.text_1'];
+  text_small = ['trainings.women.text_1'];
   // images = ['aboutme.jpg', 'stage.jpg', 'poem.png'];
   // text_big = ['home.slide_1', 'home.slide_2', 'home.slide_3'];
   // headline = ['home.box_1', 'home.box_2', 'home.box_3'];
@@ -34,22 +33,5 @@ export class SlideshowComponent implements OnInit {
         this.showImage = true;
       }, 15);
     }, 200000);
-  }
-
-  goToPart(fragment: string) {
-    const [path, anchor] = fragment.split('#');
-    this.router.navigate([path], { fragment: anchor }).then(() => {
-      // Wait a short period for navigation to complete before trying to scroll
-      setTimeout(() => {
-        const element = document.getElementById(anchor);
-        if (element) {
-          element.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start',
-            inline: 'nearest',
-          });
-        }
-      }, 200); // Delay ensures content is loaded
-    });
   }
 }
