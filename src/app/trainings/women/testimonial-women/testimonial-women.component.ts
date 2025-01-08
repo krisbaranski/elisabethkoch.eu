98207a618fe908a4c4a799ee09a031ccd34b335b
Kris Baranski <75210163+krisbaranski@users.noreply.github.com>
2025-01-08 21:14:43 +0100

    refactor: create reusable hero and testimonial

--- src/app/trainings/women/testimonial-women/testimonial-women.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-testimonial-women',
-  templateUrl: './testimonial-women.component.html',
-  styleUrl: './testimonial-women.component.scss',
-})
-export class TestimonialWomenComponent {}
