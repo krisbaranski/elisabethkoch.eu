113a87a1b3fde3b1b6bd8cef619ad19b704b55b0
Kris Baranski <75210163+krisbaranski@users.noreply.github.com>
2025-01-08 16:20:29 +0100

    refactor: add new components

---
+++ src/app/trainings/singer/singer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-singer',
+  standalone: true,
+  imports: [],
+  templateUrl: './singer.component.html',
+  styleUrl: './singer.component.scss'
+})
+export class SingerComponent {
+
+}
---
+++ src/app/trainings/speaker/speaker.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-speaker',
+  standalone: true,
+  imports: [],
+  templateUrl: './speaker.component.html',
+  styleUrl: './speaker.component.scss'
+})
+export class SpeakerComponent {
+
+}
