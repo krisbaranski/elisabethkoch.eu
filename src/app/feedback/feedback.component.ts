97b7e17467a4999d21651fcf82ffb018dd7173a8
Kris Baranski <75210163+krisbaranski@users.noreply.github.com>
2025-01-10 09:44:12 +0100

    refactor: drop reusable component due to link issue

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-feedback',
   templateUrl: './feedback.component.html',
   styleUrl: './feedback.component.scss',
 })
-export class FeedbackComponent {}
+export class FeedbackComponent {
+  @Input() text: string;
+  @Input() author: string;
+  @Input() link: string;
+  @Input() pagename: string;
+}
