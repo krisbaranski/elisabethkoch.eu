77eb2225f9f92608ada937dcb730769c7f8126ff
Kris Baranski <75210163+krisbaranski@users.noreply.github.com>
2025-01-10 09:43:14 +0100

    refactor: reusable component fix, readjust links

---
+++ src/app/home/feedbacks/feedbacks.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feedbacks',
+  templateUrl: './feedbacks.component.html',
+  styleUrl: './feedbacks.component.scss',
+})
+export class FeedbacksComponent {}
