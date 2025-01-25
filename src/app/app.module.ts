b1b36d92d6dba57a82b0e3145424f141f61f8d0b
Kris Baranski <75210163+krisbaranski@users.noreply.github.com>
2025-01-25 14:39:45 +0100

    fix: enabled fixed component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -130,7 +130,6 @@ export function HttpLoaderFactory(http: HttpClient) {
     MatMenuModule,
     ReactiveFormsModule,
     MatFormFieldModule,
-    SingleTrainingComponent,
   ],
   providers: [
     HttpClientModule,
