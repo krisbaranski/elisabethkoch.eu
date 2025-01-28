c3804e8e7e5ef10063ebb719dc5a3163a094e6e7
Kris Baranski <75210163+krisbaranski@users.noreply.github.com>
2025-01-28 17:19:28 +0100

    refactor: add subcomponents, adjust for mobile

--- src/app/profile/work/work.component.ts
+++ src/app/profile/work/work.component.ts
@@ -1,40 +1,8 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-work',
   templateUrl: './work.component.html',
   styleUrl: './work.component.scss',
 })
-export class WorkComponent {
-  text_7: string[];
-  text_8: string[];
-
-  constructor(public translate: TranslateService, public router: Router) {
-    this.translate.get('profile.work.text_7').subscribe((res: string[]) => {
-      this.text_7 = res;
-    });
-
-    this.translate.get('profile.work.text_8').subscribe((res: string[]) => {
-      this.text_8 = res;
-    });
-  }
-
-  goToPart(fragment: string) {
-    const [path, anchor] = fragment.split('#');
-    this.router.navigate([path], { fragment: anchor }).then(() => {
-      // Wait a short period for navigation to complete before trying to scroll
-      setTimeout(() => {
-        const element = document.getElementById(anchor);
-        if (element) {
-          element.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start',
-            inline: 'nearest',
-          });
-        }
-      }, 200); // Delay ensures content is loaded
-    });
-  }
-}
+export class WorkComponent {}
