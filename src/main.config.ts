b39ec08551c48d86721e62478cc2d4104df22564
Kris Baranski <75210163+krisbaranski@users.noreply.github.com>
2025-04-08 19:46:53 +0200

    angular restore ssr

--- src/main.config.ts
+++ src/main.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { appRoutes } from './app/app.routes';
+import { routes } from './app/app.routes';
 import { importProvidersFrom } from '@angular/core';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
@@ -8,7 +8,7 @@ import { TranslateModule } from '@ngx-translate/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(appRoutes),
+    provideRouter(routes),
     provideHttpClient(),
     provideAnimations(),
     importProvidersFrom(TranslateModule.forRoot()),
