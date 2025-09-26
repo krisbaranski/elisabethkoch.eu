2ddf203da593febd9b4597cea53398f0e63889cc
Kris Baranski <75210163+krisbaranski@users.noreply.github.com>
2025-09-26 09:56:45 +0200

    fix: scroll issue on mobile

--- src/app/contact/tidycal-30/tidycal-30.component.ts
+++ src/app/contact/tidycal-30/tidycal-30.component.ts
@@ -1,8 +1,66 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  NgZone,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-tidycal-30',
   templateUrl: './tidycal-30.component.html',
   styleUrl: './tidycal-30.component.scss',
 })
-export class Tidycal30Component {}
+export class Tidycal30Component implements AfterViewInit {
+  @ViewChild('tidycalFrame', { static: true })
+  tidycalFrame!: ElementRef<HTMLIFrameElement>;
+
+  private lastHeight = 0;
+  private pollingIntervalId: any;
+
+  constructor(private ngZone: NgZone) {}
+
+  ngAfterViewInit(): void {
+    this.listenForPostMessages();
+    this.startHeightPollingFallback();
+  }
+
+  private listenForPostMessages(): void {
+    window.addEventListener('message', (event: MessageEvent) => {
+      if (
+        typeof event.data === 'string' &&
+        event.data.includes('booking-form')
+      ) {
+        this.scrollToIframe();
+      }
+    });
+  }
+
+  private startHeightPollingFallback(): void {
+    this.ngZone.runOutsideAngular(() => {
+      this.pollingIntervalId = setInterval(() => {
+        const iframe = this.tidycalFrame.nativeElement;
+        const newHeight = iframe.clientHeight;
+
+        if (newHeight > this.lastHeight + 200) {
+          this.scrollToIframe();
+        }
+
+        this.lastHeight = newHeight;
+      }, 1000);
+    });
+  }
+
+  private scrollToIframe(): void {
+    this.tidycalFrame.nativeElement.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.pollingIntervalId) {
+      clearInterval(this.pollingIntervalId);
+    }
+  }
+}
