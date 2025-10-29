bca7475aeda1e4e4c8f1851540941962b40927f6
Kris Baranski <75210163+krisbaranski@users.noreply.github.com>
2025-10-29 10:50:47 +0100

    refactor: add CTA button

--- src/app/trainings/we-work/we-work.component.ts
+++ src/app/trainings/we-work/we-work.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-we-work',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrl: './we-work.component.scss',
 })
 export class WeWorkComponent {
-  constructor() {}
+  @Input() titleMedium = '';
+  @Input() titleBig = '';
+  @Input() text = '';
+  @Input() link = '';
+  @Input() button = '';
+  @Input() showButton: boolean = true; // Control visibility
+
+  constructor(private router: Router) {}
+
+  // goToPart(fragment: string) {
+  //   const [path, anchor] = fragment.split('#');
+  //   this.router.navigate([path], { fragment: anchor }).then(() => {
+  //     // Wait a short period for navigation to complete before trying to scroll
+  //     setTimeout(() => {
+  //       const element = document.getElementById(anchor);
+  //       if (element) {
+  //         element.scrollIntoView({
+  //           behavior: 'smooth',
+  //           block: 'start',
+  //           inline: 'nearest',
+  //         });
+  //       }
+  //     }, 200); // Delay ensures content is loaded
+  //   });
+  // }
 }
