240322c79103a7bab3e61d6942e5956c1812b4cd
Kris Baranski <75210163+krisbaranski@users.noreply.github.com>
2026-05-21 10:21:59 +0200

    Configure Vercel SSR deployment

--- api/index.js
+++ api/index.js
@@ -1,3 +1,5 @@
-export default import('../dist/elisabethkoch.eu/server/main.server.mjs').then(
-  (module) => module.reqHandler || module.app(),
-);
+export default async (req, res) => {
+  const { reqHandler } =
+    await import('../dist/elisabethkoch.eu/server/main.server.mjs');
+  return reqHandler(req, res);
+};
